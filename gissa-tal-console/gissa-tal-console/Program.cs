bc42f7eb747177e3ac4d3a9411d6ca079fcd4b34
mefe03001 <70005273+mefe03001@users.noreply.github.com>
2020-11-18 22:38:09 +0000

    Lade till kolsolversionen

---
+++ gissa-tal-console/gissa-tal-console/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gissa_tal_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random slump = new Random();
+            int slumptal = ((+slump.Next(100)) + (1));
+            int gissningar = 0;
+            int gissning;
+            do
+            {
+                Console.Write("Gissa ett tal mellan 1 och 100: ");
+                gissning = int.Parse(Console.ReadLine());
+                if (gissning == slumptal)
+                {
+                    gissningar++;
+                    Console.WriteLine("Talet du gissade var rätt!");
+                    Console.WriteLine("Det tog " + gissningar + " försök för dig att gissa rätt.");
+                }
+                else if (gissning < slumptal)
+                {
+                    gissningar++;
+                    Console.WriteLine("Talet du gissade var mindre än slumptalet. Försök igen.");
+                }
+                else if (gissning > slumptal)
+                {
+                    gissningar++;
+                    Console.WriteLine("Talet du gissade var större än slumptalet. Försök igen.");
+                }
+            }
+            while (gissning != slumptal);
+        }
+    }
+}
